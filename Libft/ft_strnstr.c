20af92d73f96e97f954973f0ddd6db9ce7c61914
Rafael Parra Trabalon <rparra-t@c2r8s2.42malaga.com>
2022-09-29 20:06:25 +0200

    20

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -6,33 +6,35 @@
 /*   By: rparra-t <rparra-t@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 19:01:04 by rparra-t          #+#    #+#             */
-/*   Updated: 2022/09/29 19:11:19 by rparra-t         ###   ########.fr       */
+/*   Updated: 2022/09/29 20:04:43 by rparra-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	unsigned int	cont1;
 	unsigned int	cont2;
-	unsigned char	*s1;
-	unsigned char	*s2;
+	char			*s1;
+	char			*s2;
 
-	s1 = (unsigned char *)haystack;
-	s2 = (unsigned char *)needle;
+	s1 = (char *)haystack;
+	s2 = (char *)needle;
 	cont1 = 0;
+	if (s2[0] == '\0')
+		return (s1);
 	while (s1[cont1] != '\0')
 	{
 		cont2 = 0;
 		while (s1[cont1 + cont2] == s2[cont2]
 			&& s1[cont1 + cont2] != '\0')
 		{
-			if (s2[cont2 + 1] == '\0')
+			if (s2[cont2 + 1] == '\0' && cont1 + cont2 < len)
 				return (&s1[cont1]);
 			cont2++;
 		}
 		cont1++;
 	}
-	if (s2[0] == '\0')
-		return (s1);
 	return (0);
 }
